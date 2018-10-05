ab4a8dab236e0c57e87c3987a38591188b7f0052
luan on mac pro <vominhluan88@gmail.com>
2018-10-05 23:04:09 +0700

    Update dockerfiles and others to refer to our own repo.

--- util/vpc-tools/abbey.py
+++ util/vpc-tools/abbey.py
@@ -297,7 +297,7 @@ if [[ -z "$ANSIBLE_VERSION" ]]; then
 fi
 
 if [[ -z "$CONFIGURATION_REPO" ]]; then
-  CONFIGURATION_REPO="https://github.com/edx/configuration.git"
+  CONFIGURATION_REPO="https://github.com/vmluan/edx-configuration.git"
 fi
 
 if [[ -z "$CONFIGURATION_VERSION" ]]; then
