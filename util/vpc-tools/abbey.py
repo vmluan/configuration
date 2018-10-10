987a95de987b3ae35e96c9e4fdad90eb7d3f22e4
luan on mac pro <vominhluan88@gmail.com>
2018-10-10 07:32:22 +0700

    edx-configuration

--- util/vpc-tools/abbey.py
+++ util/vpc-tools/abbey.py
@@ -313,7 +313,7 @@ if [[ -z "$ANSIBLE_VERSION" ]]; then
 fi
 
 if [[ -z "$CONFIGURATION_REPO" ]]; then
-  CONFIGURATION_REPO="https://github.com/edx/configuration.git"
+  CONFIGURATION_REPO="https://github.com/vmluan/configuration.git"
 fi
 
 if [[ -z "$CONFIGURATION_VERSION" ]]; then
